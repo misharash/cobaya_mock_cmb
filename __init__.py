02b711b398552144b559d220ca4a15a63953cb21
Michael Rashkovetskyi <misharash@gmail.com>
2021-01-27 12:36:21 +0200

    Added root package file

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+Mock CMB likelihoods for Cobaya
+"""
+
+# Base class
+from .mock_cmb_likelihood import MockCMBLikelihood
+
+# Particular examples
+from .mock_Planck import MockPlanck
+from .mock_SO import MockSO
+from .mock_SO_baseline import MockSOBaseline
+from .mock_SO_goal import MockSOGoal
+from .mock_CMBS4 import MockCMBS4
+from .mock_CMBS4sens0 import MockCMBS4sens0
