5208aac11b9648a3e4f9b2152149162a4d1e1731
Michael Rashkovetskyi <misharash@gmail.com>
2021-12-16 19:23:25 +0200

    Added setup script and metadata

--- cobaya_mock_cmb/__init__.py
+++ cobaya_mock_cmb/__init__.py
@@ -14,3 +14,10 @@ from .mock_CMBS4 import MockCMBS4
 from .mock_CMBS4sens0 import MockCMBS4sens0
 from .mock_SO_clumping import MockSOClumping
 from .mock_CMBS4_clumping import MockCMBS4Clumping
+
+# Metadata
+__author__ = "Michael Rashkovetskyi, Julian B. Muñoz, Daniel J. Eisenstein and Cora Dvorkin"
+__version__ = "0.1.0"
+__obsolete__ = False
+__year__ = "2021"
+__url__ = "https://github.com/misharash/cobaya_mock_cmb"
